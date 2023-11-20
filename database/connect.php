ce370f8616df4d654ae01600995a84d60ec9d1d3
Dilshad <annonym208@gmail.com>
2023-11-20 12:05:04 +0200

    database toegevoegd nav bar aangemaakt en meer pages aangemaakt

---
+++ database/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root"; // default XAMPP username
+$password = ""; // default XAMPP password (empty by default)
+$dbname = "klachtindienen";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
