1b7b6848e80d98b356eb26bbb58ef2ff6bcabc6c
Dilshad <annonym208@gmail.com>
2023-11-23 12:47:37 +0200

    <TEST NIET ZEKER OF WE DIT GEBRUIKEN!?!?!?>>>
    ik krijg een map te zien in mijn dashboard.php waarbij de markers er ook staan ik moet nu alleen fixen dat wanneer ik een klacht stuur dat het via ip tracking in de map komt te zien met markers

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -12,10 +12,41 @@ include('../include/navbar.html'); // Connect to the database
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Complaints Dashboard</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
 </head>
 <body>
 
 <h1>Klachten omgeving gemeente Rotterdam:</h1>
+
+<div id="map" style="height: 400px;"></div>
+
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script>
+    // Hier voeg je JavaScript toe om de map en markers weer te geven.
+
+    // Initieer de kaart op een bepaald element met een specifieke locatie en zoomniveau.
+    var map = L.map('map').setView([51.9225, 4.47917], 12); // De coördinaten hier zijn voor Rotterdam.
+
+    // Voeg de OpenStreetMap-laag toe aan de kaart.
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Hier zou je een AJAX-verzoek kunnen doen om de locatiegegevens op te halen.
+    // Vervang dit met de daadwerkelijke gegevens die je van de server hebt ontvangen.
+    var locations = [
+        { lat: 51.9225, lon: 4.47917, name: "Marker 1" },
+        { lat: 51.9125, lon: 4.46917, name: "Marker 2" },
+        { lat: 51.9125, lon: 4.56977, name: "Marker 3" },
+        // Voeg meer locaties toe zoals nodig
+    ];
+
+    // Itereer over de locaties en voeg markers toe aan de kaart.
+    locations.forEach(function(location) {
+        var marker = L.marker([location.lat, location.lon]).addTo(map);
+        marker.bindPopup(location.name);
+    });
+</script>
 
 </body>
 </html>
