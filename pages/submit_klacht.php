20fd3e5f0545023dda32e89025fb7428b48512d9
Dilshad <annonym208@gmail.com>
2023-11-24 11:52:13 +0200

    de long en lat worden nu opgeslagen in de database met behulp van hidden vallues

--- pages/submit_klacht.php
+++ pages/submit_klacht.php
@@ -1,4 +1,27 @@
 <?php
+include('../database/connect.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verwerk de klachtgegevens
+    $klacht = $_POST['klacht'];
+    $latitude = $_POST['latitude'];
+    $longitude = $_POST['longitude'];
+
+    // Voeg de klacht en locatiegegevens toe aan de database
+    $query = "INSERT INTO klachten (klacht, latitude, longitude) VALUES ('$klacht', '$latitude', '$longitude')";
+    mysqli_query($conn, $query);
+
+    // Stuur een succesvol antwoord terug (kan worden aangepast op basis van je behoeften)
+    echo json_encode(['status' => 'success']);
+} else {
+    // Stuur een foutantwoord terug als het geen POST-verzoek is
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+}
+?>
+
+
+
+<?php
 include('../database/connect.php'); // Connect to the database
 include('../include/navbar.php'); // Connect to the database
 ?>
@@ -32,5 +55,31 @@ include('../include/navbar.php'); // Connect to the database
 
 </body>
 </html>
+
+<script>
+    // Functie om de locatie van de gebruiker op te halen en deze in de hidden inputs te plaatsen
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                console.log(latitude)
+                console.log(longitude)
+                document.getElementById("latitude").value = latitude;
+                document.getElementById("longitude").value = longitude;
+
+                // Plaats de longitudinaal en latitudinaal in de hidden inputs
+                $('#latitude').val(latitude);
+                $('#longitude').val(longitude);
+            }, function(error) {
+                console.error('Fout bij het ophalen van locatie:', error.message);
+            });
+        } else {
+            console.error('Geolocatie wordt niet ondersteund in deze browser.');
+        }
+    }
+
+    getLocation();
+</script>
 
 
